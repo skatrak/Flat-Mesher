bef068d765a9b37ebf278417215a3744cd42512e
skatrak <theSkatrak@gmail.com>
2015-07-23 19:01:23 +0200

    Fixed the way in which configuration constants were specified

--- include/Configuration.h
+++ include/Configuration.h
@@ -3,26 +3,28 @@
 
 namespace config {
 
-static const int HEADER_FONT_SZ = 10;
+extern const char* VERSION_STRING;
 
-static const double MIN_TRIANGLE_SZ = 0.05;
-static const double MAX_TRIANGLE_SZ = 50.0;
-static const double DELTA_TRIANGLE_SZ = 0.1;
-static const double DEFAULT_TRIANGLE_SZ = 0.5;
+extern const int HEADER_FONT_SZ;
 
-static const double MIN_WALLS_HEIGHT = 0.05;
-static const double MAX_WALLS_HEIGHT = 500.0;
-static const double DELTA_WALLS_HEIGHT = 1.0;
-static const double DEFAULT_WALLS_HEIGHT = 2.0;
+extern const double MIN_TRIANGLE_SZ;
+extern const double MAX_TRIANGLE_SZ;
+extern const double DELTA_TRIANGLE_SZ;
+extern const double DEFAULT_TRIANGLE_SZ;
 
-static const double VIEWPORT_MIN_X = -500000.00;
-static const double VIEWPORT_MAX_X = 500000.00;
-static const double VIEWPORT_MIN_Y = -500000.00;
-static const double VIEWPORT_MAX_Y = 500000.00;
-static const double DEFAULT_VIEWPORT_MIN_X = -5.0;
-static const double DEFAULT_VIEWPORT_MAX_X = 5.0;
-static const double DEFAULT_VIEWPORT_MIN_Y = -5.0;
-static const double DEFAULT_VIEWPORT_MAX_Y = 5.0;
+extern const double MIN_WALLS_HEIGHT;
+extern const double MAX_WALLS_HEIGHT;
+extern const double DELTA_WALLS_HEIGHT;
+extern const double DEFAULT_WALLS_HEIGHT;
+
+extern const double VIEWPORT_MIN_X;
+extern const double VIEWPORT_MAX_X;
+extern const double VIEWPORT_MIN_Y;
+extern const double VIEWPORT_MAX_Y;
+extern const double DEFAULT_VIEWPORT_MIN_X;
+extern const double DEFAULT_VIEWPORT_MAX_X;
+extern const double DEFAULT_VIEWPORT_MIN_Y;
+extern const double DEFAULT_VIEWPORT_MAX_Y;
 
 } // namespace config
 
